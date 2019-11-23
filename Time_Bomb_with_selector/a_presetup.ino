bc393f00468e525837c7b59aa4cd3ae8aaa6b4f1
CommandBlock6417 <58105634+CommandBlock6417@users.noreply.github.com>
2019-11-23 12:10:19 +0200

    Add files via upload

---
+++ Time_Bomb_with_selector/a_presetup.ino
@@ -0,0 +1,45 @@
+#include <LiquidCrystal.h> // LiquidCrystal can be found on Tools > manage libraries
+#include <CountUpDownTimer.h> //CountUpDownTimer by AndrewMascolo can be found on his github page: https://github.com/AndrewMascolo/CountUpDownTimer
+const int buzzerPin = 9; //assigns the pin the buzzer is in
+const int rs = 12, en = 11, d4 = 4, d5 = 5, d6 = 6, d7 = 7; //assigns the appropriate pins to names for the lcd pins
+const int knobPin = A0; //assigns the analog pin the potentiometer is in
+const int buttonPin = 2; //assigns the pin the start button is in
+int buzzFreq = 100; //initial buzzer frequency for the siren sequence
+int buttonState = 0; //creates a new variable where the state of the button will be stored
+int timeSecs; //creates a new variable where the value of the potentiometer is converted to seconds
+float knobReading; //creates a decimal point variable for the potentiometers value to be stored
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7); //lcd register select, enable and data pins 4-7 are used by the lcd
+CountUpDownTimer T(DOWN); //makes a new timer named "T" and sets it to countdown
+void timeSet(){
+  buttonState = digitalRead(buttonPin);
+  knobReading = analogRead(knobPin);
+  timeSecs = ((knobReading * 1024) / 1745);
+  Serial.print("time is set to: ");
+  Serial.print(timeSecs);
+  Serial.println(" seconds");
+
+
+}
+void beepity(){ //does the beeping sound every second
+      tone(buzzerPin, 3000);
+      delay(100);
+      noTone(buzzerPin);
+      delay(900);
+    }
+void siren(){ //does the siren sound after the time is up
+  do{ //increments the frequency
+   buzzFreq = buzzFreq + 20;
+   tone(buzzerPin,buzzFreq);
+   Serial.println(buzzFreq);
+   Serial.print("frequency:");
+   delay(1);
+  } while (buzzFreq < 3500);
+
+  do{ //decrements the frequency
+   buzzFreq = buzzFreq - 20;
+   tone(buzzerPin,buzzFreq);
+   Serial.println(buzzFreq);
+   Serial.print("frequency:");
+   delay(1);
+  } while (buzzFreq > 100);
+}
---
+++ Time_Bomb_with_selector/b_setup.ino
@@ -0,0 +1,19 @@
+void setup() { 
+    pinMode(buzzerPin,OUTPUT);
+    Serial.begin(9600);//initiates serial communications
+    Serial.println(); //clears the serial monitor
+    lcd.begin(16,2);  //columns and rows of the lcd display 
+    do {
+     timeSet(); //refreshes the values of the button and potentiometer
+     lcd.setCursor(0, 0); //col, row
+     lcd.print("Select time:");
+     lcd.setCursor(0, 1); //col, row
+     lcd.print(timeSecs);
+     lcd.print(" Seconds");
+    } while(buttonState == 0); 
+    lcd.clear();   
+    T.SetTimer(timeSecs); //sets the timer to the value of the potentiometer
+    T.StartTimer();  //start the timer
+    Serial.println("Countdown initiated"); //marks that the countdown has started
+           
+}
